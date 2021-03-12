dc3c49c3bfceb91321d66963870ae3d0552fb9d0
bonjorno7 <jorijndegraaf@gmail.com>
2021-03-12 03:34:38 +0100

    Implement function oriented SMD encoder
    Just trying stuff.

--- sdk/api/smd.py
+++ sdk/api/smd.py
@@ -11,123 +11,102 @@ from ..models.smd import (
 )
 
 
-class SMDSerializer(object):
-    '''SMD serializer.'''
+def encode_smd(smd):
+    # type: (SMDModel) -> str
+    smd = deepcopy(smd)
 
-    def __init__(self, smd):
-        # type: (SMDModel) -> None
-        '''Serialize SMD.'''
-        self._smd = deepcopy(smd)
-        self._lines = []
+    return 'version {}\n{}{}{}'.format(
+        smd.version,
+        _encode_nodes(smd.nodes),
+        _encode_frames(smd.frames),
+        _encode_triangles(smd.triangles),
+    )
 
-        self._serialize_version(self._smd.version)
-        self._serialize_nodes(self._smd.nodes)
-        self._serialize_frames(self._smd.frames)
-        self._serialize_triangles(self._smd.triangles)
 
-        self._lines.append('')
-        self._string = '\n'.join(self._lines)
+def _encode_nodes(nodes):
+    # type: (List[SMDNodeModel]) -> str
+    if not nodes:
+        nodes.append(SMDNodeModel())
 
-    def __str__(self):
-        # type: () -> str
-        '''Get SMD string.'''
-        return self._string
+    return 'nodes\n{}end\n'.format(''.join(
+        _encode_node(node) for node in nodes))
 
-    def _serialize_version(self, version):
-        # type: (int) -> None
-        self._lines.append('version {}'.format(version))
 
-    def _serialize_nodes(self, nodes):
-        # type: (List[SMDNodeModel]) -> None
-        self._lines.append('nodes')
+def _encode_node(node):
+    # type: (SMDNodeModel) -> str
+    return '{}    "{}"    {}\n'.format(
+        node.index,
+        node.name,
+        node.parent,
+    )
 
-        if not nodes:
-            nodes.append(SMDNodeModel())
 
-        for node in nodes:
-            self._serialize_node(node)
+def _encode_frames(frames):
+    # type: (List[SMDFrameModel]) -> str
+    if not frames:
+        frames = [SMDFrameModel()]
 
-        self._lines.append('end')
+    return 'skeleton\n{}end\n'.format(''.join(
+        _encode_frame(frame) for frame in frames))
 
-    def _serialize_node(self, node):
-        # type: (SMDNodeModel) -> None
-        self._lines.append('{}    "{}"    {}'.format(
-            node.index,
-            node.name,
-            node.parent,
-        ))
 
-    def _serialize_frames(self, frames):
-        # type: (List[SMDFrameModel]) -> None
-        self._lines.append('skeleton')
+def _encode_frame(frame):
+    # type: (SMDFrameModel) -> str
+    if not frame.bones:
+        frame.bones.append(SMDBoneModel())
 
-        if not frames:
-            frames.append(SMDFrameModel())
+    return 'time {}\n{}'.format(
+        frame.time,
+        ''.join(_encode_bone(bone) for bone in frame.bones),
+    )
 
-        for frame in frames:
-            self._serialize_frame(frame)
 
-        self._lines.append('end')
+def _encode_bone(bone):
+    # type: (SMDBoneModel) -> str
+    return '{}    {}    {}\n'.format(
+        bone.index,
+        _encode_vector(bone.pos),
+        _encode_vector(bone.rot),
+    )
 
-    def _serialize_frame(self, frame):
-        # type: (SMDFrameModel) -> None
-        self._lines.append('time {}'.format(frame.time))
 
-        if not frame.bones:
-            frame.bones.append(SMDBoneModel())
+def _encode_triangles(triangles):
+    # type: (List[SMDTriangleModel]) -> str
+    if not triangles:
+        return ''
 
-        for bone in frame.bones:
-            self._serialize_bone(bone)
+    return 'triangles\n{}end\n'.format(''.join(
+        _encode_triangle(triangle) for triangle in triangles))
 
-    def _serialize_bone(self, bone):
-        # type: (SMDBoneModel) -> None
-        self._lines.append('{}    {}    {}'.format(
-            bone.index,
-            self._convert_vector(bone.pos),
-            self._convert_vector(bone.rot),
-        ))
 
-    def _serialize_triangles(self, triangles):
-        # type: (List[SMDTriangleModel]) -> None
-        if not triangles:
-            return
+def _encode_triangle(triangle):
+    # type: (SMDTriangleModel) -> str
+    return '{}\n{}'.format(
+        triangle.material,
+        ''.join(_encode_vertex(vertex) for vertex in triangle.vertices),
+    )
 
-        self._lines.append('triangles')
 
-        for triangle in triangles:
-            self._serialize_triangle(triangle)
+def _encode_vertex(vertex):
+    # type: (SMDVertexModel) -> str
+    if not vertex.weights:
+        vertex.weights.append((0, 1))
 
-        self._lines.append('end')
+    return '{}    {}  {}  {}    {}    {}\n'.format(
+        vertex.parent,
+        _encode_vector(vertex.pos),
+        _encode_vector(vertex.nor),
+        _encode_vector(vertex.uv),
+        len(vertex.weights),
+        _encode_weights(vertex.weights),
+    )
 
-    def _serialize_triangle(self, triangle):
-        # type: (SMDTriangleModel) -> None
-        self._lines.append(triangle.material)
 
-        for vertex in triangle.vertices:
-            self._serialize_vertex(vertex)
+def _encode_vector(vec):
+    # type: (Iterable[float]) -> str
+    return ' '.join(str(round(n, 6)) for n in vec)
 
-    def _serialize_vertex(self, vertex):
-        # type: (SMDVertexModel) -> None
-        if not vertex.weights:
-            vertex.weights.append((0, 1))
 
-        self._lines.append('{}    {}  {}  {}    {}    {}'.format(
-            vertex.parent,
-            self._convert_vector(vertex.pos),
-            self._convert_vector(vertex.nor),
-            self._convert_vector(vertex.uv),
-            len(vertex.weights),
-            self._convert_weights(vertex.weights),
-        ))
-
-    def _convert_vector(self, vec):
-        # type: (Iterable[float]) -> str
-        return ' '.join(str(round(n, 6)) for n in vec)
-
-    def _convert_weights(self, weights):
-        # type: (List[Tuple[int, float]]) -> str
-        return '  '.join(self._convert_weight(i, w) for i, w in weights)
-
-    def _convert_weight(self, index, weight):
-        # type: (int, float) -> str
-        return '{} {}'.format(index, round(weight, 6))
+def _encode_weights(weights):
+    # type: (List[Tuple[int, float]]) -> str
+    return '  '.join('{} {}'.format(i, round(w, 6)) for i, w in weights)
