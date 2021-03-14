1796c9b460aa3f19b4e437b8d82db4dbaead8761
bonjorno7 <jorijndegraaf@gmail.com>
2021-03-14 21:14:42 +0100

    Make encoder methods private
    It's kinda neat that you can use all of these independently, but I don't think I'll be able to do that for the decoder, and I don't want their features to be different.
    Besides, not like I actually expect anyone to use anything other than the main encode / decode methods.
    Oh and uh the order of some lines got a bit messed up last commit, so I'm fixing that in this commit.

--- sdk/api/smd.py
+++ sdk/api/smd.py
@@ -20,117 +20,117 @@ class SMDEncoder(object):
     def encode_smd(self, smd):
         # type: (SMDModel) -> str
         return ''.join((
-            self.encode_version(smd.version),
-            self.encode_nodes(smd.nodes),
-            self.encode_frames(smd.frames),
-            self.encode_triangles(smd.triangles),
+            self._encode_version(smd.version),
+            self._encode_nodes(smd.nodes),
+            self._encode_frames(smd.frames),
+            self._encode_triangles(smd.triangles),
         ))
 
-    def encode_version(self, version):
+    def _encode_version(self, version):
         # type: (int) -> str
-        version = self.encode_number(version)
+        version = self._encode_number(version)
         return 'version {}\n'.format(version)
 
-    def encode_nodes(self, nodes):
+    def _encode_nodes(self, nodes):
         # type: (List[SMDNodeModel]) -> str
         if self.add_default_values and not nodes:
             nodes = [SMDNodeModel()]
 
-        nodes = ''.join(map(self.encode_node, nodes))
+        nodes = ''.join(map(self._encode_node, nodes))
         return 'nodes\n{}end\n'.format(nodes)
 
-    def encode_node(self, node):
+    def _encode_node(self, node):
         # type: (SMDNodeModel) -> str
         return '{}    "{}"    {}\n'.format(
-            self.encode_number(node.index),
+            self._encode_number(node.index),
             node.name,
             node.parent,
         )
 
-    def encode_frames(self, frames):
+    def _encode_frames(self, frames):
         # type: (List[SMDFrameModel]) -> str
         if self.add_default_values and not frames:
             frames = [SMDFrameModel()]
 
-        frames = ''.join(map(self.encode_frame, frames))
+        frames = ''.join(map(self._encode_frame, frames))
         return 'skeleton\n{}end\n'.format(frames)
 
-    def encode_frame(self, frame):
+    def _encode_frame(self, frame):
         # type: (SMDFrameModel) -> str
         return ''.join((
-            self.encode_time(frame.time),
-            self.encode_bones(frame.bones),
+            self._encode_time(frame.time),
+            self._encode_bones(frame.bones),
         ))
 
-    def encode_time(self, time):
+    def _encode_time(self, time):
         # type: (int) -> str
-        time = self.encode_number(time)
+        time = self._encode_number(time)
         return 'time {}\n'.format(time)
 
-    def encode_bones(self, bones):
+    def _encode_bones(self, bones):
         # type: (List[SMDBoneModel]) -> str
         if self.add_default_values and not bones:
             bones = [SMDBoneModel()]
 
-        return ''.join(map(self.encode_bone, bones))
+        return ''.join(map(self._encode_bone, bones))
 
-    def encode_bone(self, bone):
+    def _encode_bone(self, bone):
         # type: (SMDBoneModel) -> str
         return '{}    {}    {}\n'.format(
-            self.encode_number(bone.index),
-            self.encode_vector(bone.pos),
-            self.encode_vector(bone.rot),
+            self._encode_number(bone.index),
+            self._encode_vector(bone.pos),
+            self._encode_vector(bone.rot),
         )
 
-    def encode_triangles(self, triangles):
+    def _encode_triangles(self, triangles):
         # type: (List[SMDTriangleModel]) -> str
         if not triangles:
             return ''
 
-        triangles = ''.join(map(self.encode_triangle, triangles))
+        triangles = ''.join(map(self._encode_triangle, triangles))
         return 'triangles\n{}end\n'.format(triangles)
 
-    def encode_triangle(self, triangle):
+    def _encode_triangle(self, triangle):
         # type: (SMDTriangleModel) -> str
         return ''.join((
-            self.encode_material(triangle.material),
-            self.encode_vertices(triangle.vertices),
+            self._encode_material(triangle.material),
+            self._encode_vertices(triangle.vertices),
         ))
 
-    def encode_material(self, material):
+    def _encode_material(self, material):
         # type: (str) -> str
         return '{}\n'.format(material)
 
-    def encode_vertices(self, vertices):
+    def _encode_vertices(self, vertices):
         # type: (List[SMDVertexModel]) -> str
-        return ''.join(map(self.encode_vertex, vertices))
+        return ''.join(map(self._encode_vertex, vertices))
 
-    def encode_vertex(self, vertex):
+    def _encode_vertex(self, vertex):
         # type: (SMDVertexModel) -> str
         return '{}    {}  {}  {}    {}    {}\n'.format(
-            self.encode_number(vertex.parent),
-            self.encode_vector(vertex.pos),
-            self.encode_vector(vertex.nor),
-            self.encode_vector(vertex.uv),
-            self.encode_number(len(vertex.weights)),
-            self.encode_weights(vertex.weights),
+            self._encode_number(vertex.parent),
+            self._encode_vector(vertex.pos),
+            self._encode_vector(vertex.nor),
+            self._encode_vector(vertex.uv),
+            self._encode_number(len(vertex.weights)),
+            self._encode_weights(vertex.weights),
         )
 
-    def encode_weights(self, weights):
+    def _encode_weights(self, weights):
         # type: (List[SMDWeightModel]) -> str
         if self.add_default_values and not weights:
             weights = [SMDWeightModel()]
 
-        return '  '.join(map(self.encode_weight, weights))
+        return '  '.join(map(self._encode_weight, weights))
 
-    def encode_weight(self, weight):
+    def _encode_weight(self, weight):
         # type: (SMDWeightModel) -> str
-        return self.encode_vector((weight.index, weight.value))
+        return self._encode_vector((weight.index, weight.value))
 
-    def encode_vector(self, vector):
-        return ' '.join(map(self.encode_number, vector))
+    def _encode_vector(self, vector):
         # type: (List[Union[int, float]]) -> str
+        return ' '.join(map(self._encode_number, vector))
 
-    def encode_number(self, value):
+    def _encode_number(self, value):
         # type: (Union[int, float]) -> str
         return str(round(value, 6))
