a41953f6aab42834329c9988b31f3b375f37f3eb
bonjorno7 <jorijndegraaf@gmail.com>
2021-03-15 03:21:42 +0100

    Implement SMDDecoder
    There's a glaring TODO, the variable for `_decode_number()` is kinda weird, and `decode_smd()` is a large top-down affair, but otherwise I think this is pretty nice.
    I'm going to try to think of a way to divide it into more methods, perhaps sending the iterator and calling it `commands`.
    The csv library works well, though it only separates by spaces, not tabs.
    I'm not going to worry about that right now though, because tabs can cause StudioMDL to crash, so nobody uses them anyway.

--- sdk/api/smd.py
+++ sdk/api/smd.py
@@ -1,5 +1,6 @@
 '''SMD file API.'''
 from typing import List, Union
+import csv
 from ..models.smd import (
     SMDNodeModel,
     SMDBoneModel,
@@ -134,3 +135,128 @@ class SMDEncoder(object):
     def _encode_number(self, value):
         # type: (Union[int, float]) -> str
         return str(round(value, 6))
+
+
+class SMDDecoder(object):
+
+    def __init__(self):
+        pass
+
+    def decode_smd(self, string):
+        # type: (str) -> SMDModel
+        lines = string.splitlines()
+        # TODO: Remove comments and empty lines
+        commands = csv.reader(lines, delimiter=' ')
+
+        smd = SMDModel()
+        command_type = 'pending'
+        vertex_index = 0
+
+        for command in commands:
+            if command_type == 'pending':
+                if command[0] == 'version':
+                    smd.version = self._decode_version(command)
+
+                elif command[0] == 'nodes':
+                    command_type = 'nodes'
+
+                elif command[0] == 'skeleton':
+                    command_type = 'frames'
+
+                elif command[0] == 'triangles':
+                    command_type = 'material'
+
+            elif command_type == 'nodes':
+                if command[0] == 'end':
+                    command_type = 'pending'
+
+                else:
+                    smd.nodes.append(self._decode_node(command))
+
+            elif command_type == 'frames':
+                if command[0] == 'end':
+                    command_type = 'pending'
+
+                elif command[0] == 'time':
+                    smd.frames.append(self._decode_frame(command))
+
+                else:
+                    smd.frames[-1].bones.append(self._decode_bone(command))
+
+            elif command_type == 'material':
+                smd.triangles.append(self._decode_triangle(command))
+
+                command_type = 'vertices'
+                vertex_index = 0
+
+            elif command_type == 'vertices':
+                if vertex_index > 2:
+                    command_type = 'material'
+                    vertex_index = 0
+
+                else:
+                    smd.triangles[-1].vertices.append(
+                        self._decode_vertex(command))
+
+                    command_type = 'vertices'
+                    vertex_index += 1
+
+    def _decode_version(self, command):
+        # type: (List[str]) -> int
+        _, version = command
+        return self._decode_number(version)
+
+    def _decode_node(self, command):
+        # type: (List[str]) -> SMDNodeModel
+        return SMDNodeModel(
+            index=self._decode_number(command[0]),
+            name=command[1],
+            parent=self._decode_number(command[2]),
+        )
+
+    def _decode_frame(self, command):
+        # type: (List[str]) -> SMDFrameModel
+        _, time = command
+        return SMDFrameModel(time=self._decode_number(time))
+
+    def _decode_bone(self, command):
+        # type: (List[str]) -> SMDBoneModel
+        return SMDBoneModel(
+            index=self._decode_number(command[0]),
+            pos=self._decode_vector(command[1:4]),
+            rot=self._decode_vector(command[4:7]),
+        )
+
+    def _decode_triangle(self, command):
+        # type: (List[str]) -> SMDTriangleModel
+        return SMDTriangleModel(material=command[0])
+
+    def _decode_vertex(self, command):
+        # type: (List[str]) -> SMDVertexModel
+        return SMDVertexModel(
+            parent=self._decode_number(command[0]),
+            pos=self._decode_vector(command[1:4]),
+            nor=self._decode_vector(command[4:7]),
+            weights=self._decode_weights(command[8:]),
+        )
+
+    def _decode_weights(self, command):
+        # type: (List[str]) -> List[SMDWeightModel]
+        pairs = zip(*[iter(command)] * 2)
+        return list(map(self._decode_weight, pairs))
+
+    def _decode_weight(self, command):
+        # type: (List[str]) -> SMDWeightModel
+        index, value = self._decode_vector(command)
+        return SMDWeightModel(index=index, value=value)
+
+    def _decode_vector(self, command):
+        # type: (List[str]) -> List[Union[int, float]]
+        return list(map(self._decode_number, command))
+
+    def _decode_number(self, token):
+        # type: (str) -> Union[int, float]
+        try:
+            return int(token)
+        except ValueError:
+            return float(token)
